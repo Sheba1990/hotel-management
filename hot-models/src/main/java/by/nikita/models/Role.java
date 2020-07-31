b06852f35c98595a3640d79c0d3c8cbf3a8bd791
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2020-07-31 20:03:33 +0300

    Adding Service layer

--- hot-models/src/main/java/by/nikita/models/Role.java
+++ hot-models/src/main/java/by/nikita/models/Role.java
@@ -1,16 +1,10 @@
 package by.nikita.models;
 
-import org.springframework.security.core.GrantedAuthority;
-
-public enum Role implements GrantedAuthority {
+public enum Role {
 
     USER,
     ADMIN;
 
-    @Override
-    public String getAuthority() {
-        return name();
-    }
 }
 
 
--- hot-models/src/main/java/by/nikita/models/User.java
+++ hot-models/src/main/java/by/nikita/models/User.java
@@ -1,15 +1,12 @@
 package by.nikita.models;
 
-import org.springframework.security.core.GrantedAuthority;
-
 import javax.persistence.*;
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name = "user_table")
-public class User extends AbstractIdAwareEntity implements org.springframework.security.core.userdetails.UserDetails {
+public class User extends AbstractIdAwareEntity {
 
     @Column(name = "username")
     private String username;
@@ -56,30 +53,6 @@ public class User extends AbstractIdAwareEntity implements org.springframework.s
         this.roles = roles;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return isActive();
-    }
 
     public String getUsername() {
         return username;
