c4b2c2be13d086483141b40733a6172c9becace3
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2020-08-19 12:58:13 +0300

    Adding web layer

--- hot-controllers/src/main/java/by/nikita/controllers/WelcomeController.java
+++ hot-controllers/src/main/java/by/nikita/controllers/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/welcome")
+@RequestMapping(value = "/")
 public class WelcomeController {
 
     @GetMapping
