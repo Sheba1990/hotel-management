5daa8fdd4d15caeffacff20990def14456659997
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2020-09-07 19:22:14 +0300

    Adding Web view layer Configuring security layer

--- hot-application/src/main/java/by/nikita/config/WebSecurityConfiguration.java
+++ hot-application/src/main/java/by/nikita/config/WebSecurityConfiguration.java
@@ -1,11 +1,17 @@
 package by.nikita.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SpringTemplateEngine springTemplateEngine;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,19 +20,26 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/registration").permitAll()
                 .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
                 // .antMatchers("/users", "/orders", "/rooms").access("hasRole('ADMIN')")
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/login").permitAll()
                 .usernameParameter("username")
                 .passwordParameter("password")
                 .and()
                 .csrf()
                 .and()
+                .rememberMe()
+                .rememberMeParameter("rememberme")
+                .userDetailsService(service)
+                .and()
                 .exceptionHandling()
                 .accessDeniedPage("/access_denied")
                 .and()
-                .logout()
-                .logoutSuccessUrl("/");
+                .logout().permitAll()
+                .logoutSuccessUrl("/")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"));
+
     }
 }
